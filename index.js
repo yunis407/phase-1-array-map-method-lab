d97ce4d55990c85bfebe7738a6147733e45e7387
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-03-29 14:22:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  });
 }
+
